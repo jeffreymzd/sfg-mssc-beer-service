21c87fbebf0a1d1bc42c1804dcb1466b058e5c60
jeffreymzd <mzhde@hotmail.com>
2020-03-15 19:39:10 -0400

    Add comment

--- src/test/java/com/github/jeffrey/spring/boot/sfgmsscbeerservice/SfgMsscBeerServiceApplicationTests.java
+++ src/test/java/com/github/jeffrey/spring/boot/sfgmsscbeerservice/SfgMsscBeerServiceApplicationTests.java
@@ -3,6 +3,12 @@ package com.github.jeffrey.spring.boot.sfgmsscbeerservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * DevTools to use auto restart, run in command line with below command:
+ *
+ * 		mvn spring-boot:run
+ *
+ */
 @SpringBootTest
 class SfgMsscBeerServiceApplicationTests {
 
