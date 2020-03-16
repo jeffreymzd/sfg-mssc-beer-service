72e61a185eb70a83e4079e805f25a72e76abb0da
jeffreymzd <mzhde@hotmail.com>
2020-03-16 08:55:02 -0400

    Add JPA to Beer object. Close #1. Close #2.

--- src/main/java/com/github/jeffrey/spring/boot/sfgmsscbeerservice/web/service/BeerServiceImpl.java
+++ src/main/java/com/github/jeffrey/spring/boot/sfgmsscbeerservice/web/service/BeerServiceImpl.java
@@ -1,9 +1,13 @@
 package com.github.jeffrey.spring.boot.sfgmsscbeerservice.web.service;
 
 import com.github.jeffrey.spring.boot.sfgmsscbeerservice.web.model.BeerDto;
+import com.github.jeffrey.spring.boot.sfgmsscbeerservice.web.model.BeerStyleEnum;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.Entity;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
 /**
@@ -12,10 +16,21 @@ import java.util.UUID;
 
 @Service
 @Slf4j
+@Entity
 public class BeerServiceImpl implements BeerService {
     @Override
     public BeerDto getBeerById(UUID beerId) {
-        return BeerDto.builder().id(UUID.randomUUID()).build();
+        return BeerDto.builder()
+                .id(UUID.randomUUID())
+                .beerName("Stella")
+                .beerStyle(BeerStyleEnum.PALE_ALE)
+                .createdDate(OffsetDateTime.now())
+                .lastModifiedDate(OffsetDateTime.now())
+                .price(new BigDecimal(2.5))
+                .quantityOnHand(100)
+                .upc(123456789012L)
+                .version(1)
+                .build();
     }
 
     @Override
@@ -25,11 +40,11 @@ public class BeerServiceImpl implements BeerService {
 
     @Override
     public void updateBeer(UUID beerId, BeerDto beerDto) {
-        log.debug("Updating a beer...");
+        log.info("Updating a beer: {}", beerId);
     }
 
     @Override
     public void deleteBeer(UUID beerId) {
-        log.debug("Deleting a beer...");
+        log.info("Deleting a beer: {}", beerId);
     }
 }
