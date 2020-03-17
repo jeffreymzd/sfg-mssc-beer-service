8b5560d1632c08df5666430ae25a028967a7b839
jeffreymzd <mzhde@hotmail.com>
2020-03-16 22:27:31 -0400

    Add restdocs to tests

--- src/test/java/com/github/jeffrey/spring/boot/sfgmsscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/com/github/jeffrey/spring/boot/sfgmsscbeerservice/web/controller/BeerControllerTest.java
@@ -7,9 +7,11 @@ import com.github.jeffrey.spring.boot.sfgmsscbeerservice.web.service.BeerService
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -19,13 +21,20 @@ import java.util.UUID;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*; // original import without REST Docs
 
 /**
  * Created by jeffreymzd on 3/15/20
  */
-@ExtendWith(SpringExtension.class)
+@AutoConfigureRestDocs
+@ExtendWith({SpringExtension.class, RestDocumentationExtension.class})
 @WebMvcTest(controllers = BeerController.class)
 class BeerControllerTest {
 
@@ -58,10 +67,27 @@ class BeerControllerTest {
 
         validBeer = getValidBeerDto();
 
-        mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
+        mockMvc.perform(get("/api/v1/beer/{beerId}", UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.beerName", is(validBeer.getBeerName())));
+                .andExpect(jsonPath("$.beerName", is(validBeer.getBeerName())))
+                .andDo(document("v1/beer",
+                        // documenting path parameters
+                        pathParameters(
+                                parameterWithName("beerId").description("UUID of desired beer to get.")
+                        ),
+                        // documenting response fields
+                        responseFields(
+                               fieldWithPath("id").description("Id of Beer"),
+                               fieldWithPath("version").description("Version Number"),
+                               fieldWithPath("createdDate").description("Date Created"),
+                               fieldWithPath("lastModifiedDate").description("Date Updated"),
+                               fieldWithPath("beerName").description("Beer Name"),
+                               fieldWithPath("beerStyle").description("Beer Style"),
+                               fieldWithPath("upc").description("UPC of Beer"),
+                               fieldWithPath("price").description("Price"),
+                               fieldWithPath("quantityOnHand").description("Quantity On Hand")
+                        )));
     }
 
     @Test
@@ -75,7 +101,19 @@ class BeerControllerTest {
         mockMvc.perform(post("/api/v1/beer/")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(beerDtoJson))
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andDo(document("v1/beer",
+                        requestFields(
+                               fieldWithPath("id").ignored(),
+                               fieldWithPath("version").ignored(),
+                               fieldWithPath("createdDate").ignored(),
+                               fieldWithPath("lastModifiedDate").ignored(),
+                               fieldWithPath("beerName").description("Beer Name"),
+                               fieldWithPath("beerStyle").description("Beer Style"),
+                               fieldWithPath("upc").description("UPC of Beer"),
+                               fieldWithPath("price").description("Price"),
+                               fieldWithPath("quantityOnHand").ignored()
+                        )));
     }
 
     @Test
