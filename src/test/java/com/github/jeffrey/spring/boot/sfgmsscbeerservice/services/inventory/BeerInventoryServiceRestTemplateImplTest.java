e8389ccf482a92e30490257d1e63d63defb5e22a
jeffreymzd <mzhde@hotmail.com>
2020-03-19 00:24:11 -0400

    Add disabled test tag

--- src/test/java/com/github/jeffrey/spring/boot/sfgmsscbeerservice/services/inventory/BeerInventoryServiceRestTemplateImplTest.java
+++ src/test/java/com/github/jeffrey/spring/boot/sfgmsscbeerservice/services/inventory/BeerInventoryServiceRestTemplateImplTest.java
@@ -1,5 +1,6 @@
 package com.github.jeffrey.spring.boot.sfgmsscbeerservice.services.inventory;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,6 +10,7 @@ import java.util.UUID;
 /**
  * Created by jeffreymzd on 3/19/20
  */
+@Disabled
 @SpringBootTest
 class BeerInventoryServiceRestTemplateImplTest {
 
