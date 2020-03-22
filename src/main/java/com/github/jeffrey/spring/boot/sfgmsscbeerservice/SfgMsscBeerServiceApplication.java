e01cd4f10f20d99defa8b2d2fadf8b4870ffa5c5
jeffreymzd <mzhde@hotmail.com>
2020-03-22 10:58:12 -0400

    Configure MySQL

--- src/main/java/com/github/jeffrey/spring/boot/sfgmsscbeerservice/SfgMsscBeerServiceApplication.java
+++ src/main/java/com/github/jeffrey/spring/boot/sfgmsscbeerservice/SfgMsscBeerServiceApplication.java
@@ -2,8 +2,9 @@ package com.github.jeffrey.spring.boot.sfgmsscbeerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ArtemisAutoConfiguration.class)
 public class SfgMsscBeerServiceApplication {
 
 	public static void main(String[] args) {
